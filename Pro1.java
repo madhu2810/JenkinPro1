0de51f0c68628a37dd02b8f87647cc28b11ee259
madhu2810 <madhurids2892@gmail.com>
2019-07-26 17:00:48 +0530

    First Commit

---
+++ Pro1.java
@@ -0,0 +1,12 @@
+package JenkinPro1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Pro1 {
+	@Test
+	public static void method1() {
+		Reporter.log("method1",true);
+	}
+
+}
